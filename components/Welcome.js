6469b5e14bef88e48b1c5fa921c5fe0c362d02ff
moumniheithem <moumniheithem@gmail.com>
2021-05-15 16:00:16 +0100

    fix: update masrhead

--- components/Welcome.js
+++ components/Welcome.js
@@ -1,25 +1,26 @@
 export default function Welcome() {
   return (
-    <header className='masthead bg-light page-section'>
-      <div className='container'>
-        <div div className='intro-text'>
-          <iframe
-            width="720"
-            height="380"
-            src="https://www.youtube.com/embed/ncCaVk_GhMs"
-            title="YouTube video player"
-            frameborder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowfullscreen
-          />
-          <br /><br /><br />
-          <div className='intro-lead-in'>We talk about javascript</div>
-          <div className='intro-heading text-uppercase'>Come Join Us!</div>
-          <a className='btn btn-primary btn-xl text-uppercase js-scroll-trigger' href='#about'>
+    <header className="masthead bg-light">
+      <div>
+        <iframe
+          src="https://www.youtube.com/embed/ncCaVk_GhMs"
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        />
+        {/*
+        <div className="intro-text">
+          <div className="intro-lead-in">We talk about javascript</div>
+          <div className="intro-heading text-uppercase">Come Join Us!</div>
+          <a
+            className="btn btn-primary btn-xl text-uppercase js-scroll-trigger"
+            href="#about"
+          >
             Tell Me More
           </a>
-        </div >
-      </div >
-    </header >
-  )
+        </div>*/}
+      </div>
+    </header>
+  );
 }
