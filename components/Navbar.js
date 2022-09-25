17f775880f2b4df3d3770634887e77f476e77507
3imed-jaberi <imed_jebari@hotmail.fr>
2022-09-25 22:21:18 +0200

    fix: fix nav item content --proposals

--- components/Navbar.js
+++ components/Navbar.js
@@ -81,7 +81,7 @@ export default function Navbar() {
                 rel="noreferrer"
                 href="#"
               >
-                Apply for A Talk
+                Your Proposals
               </a>
             </li>
 
